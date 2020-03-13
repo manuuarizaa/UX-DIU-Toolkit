c321ba1fcd6af3ec4e39e0a7d68a4b94f3ba909e
Ismael Moyano Romero <ismamoyano97@gmail.com>
2020-03-13 21:41:47 +0100

    Update Journey.js

--- Journey.js
+++ Journey.js
@@ -19,7 +19,7 @@
 
 angular.module("angular", [])
 	.controller("controller", ["$scope", function($scope) { 
-		$scope.Grupo_ID ="DIU2.A Título: Compañeros Reguleros ";
+		$scope.Grupo_ID ="DIU2.A Título: Compañeros Reguleros/Demasiada Info ";
         $scope.Curso ="2019/20";
         $scope.Github_ID ="https://github.com/mgea/UX-DIU-Toolkit";
         
